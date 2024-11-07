027098a0d4b120ab8a670a71854ab9b2799ace64
Ivan Kobzar <ivan.kobzar@kilo.health>
2024-11-07 08:08:48 +0200

    feat: add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { vitePlugin as remix } from "@remix-run/dev";
 
-declare module '@remix-run/node' {
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
   }
